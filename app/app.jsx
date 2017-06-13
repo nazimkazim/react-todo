b531f20ac43e277283fde0d6819b1269bb35891a
nazimkazim <nazik_111@mail.ru>
2017-06-13 14:05:33 +0600

    Init commit

---
+++ app/app.jsx
@@ -0,0 +1,20 @@
+// GreeterMessage component
+var React =require('react');
+var ReactDOM = require('react-dom');
+
+// Load foundation
+
+$(document).foundation();
+
+// App css
+require('style!css!sass!applicationStyles');
+
+ReactDOM.render(
+    <p>Boiler 3 Project</p>,
+    document.getElementById('app')
+);
+
+
+
+
+
